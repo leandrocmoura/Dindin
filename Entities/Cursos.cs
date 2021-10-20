5a377a5b6ac8dbd68ca38b7c0f7f7c14fb4977ff
leandrocmoura <custodio257@gmail.com>
2021-10-20 20:17:23 -0300

    Projeto Final Gama

---
+++ Entities/Cursos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities
+{
+    public class Cursos
+    {
+        public string Id { get; set; }
+
+        public string Url { get; set; }
+
+        public Professores Professores { get; set; }
+
+        public string Descricao { get; set; }
+
+        public List<Aulas> Aulas { get; set; }
+    }
+}
---
+++ Services/Cursos/CursoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Services.Curso
+
+{
+    public class CursoService
+
+    {
+        public Cursos Cursando()
+        {
+            return new Cursos();
+        }
+
+
+    }
+}
